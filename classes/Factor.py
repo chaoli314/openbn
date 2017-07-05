0633272bcab63a4df8a0d1fe131355dcfb966e3c
chao li <chao.li.314@gmail.com>
2017-07-05 20:11:11 +0900

    add [def marginalization(self, new_scope):] in Factor.py

--- classes/Factor.py
+++ classes/Factor.py
@@ -21,11 +21,22 @@ class Factor(object):
         self._scope = scope
         self._parameters = parameters
 
+    @property
+    def scope(self):
+        return self._scope
+
+    def marginalization(self, new_scope):
+        new_scope = sorted(new_scope)
+        i = index_for(new_scope, self._scope)
+        new_parameters = np.bincount(i, weights=self._parameters)
+        return Factor(new_scope, new_parameters)
+
+    def summing_out(self, y):
+        new_scope = sorted(list(set(self.scope) - set([y])))
+        return self.marginalization(new_scope)
+
     def __mul__(self, other):
         """ The implementation of a factor product operation.
-
-        :param other:
-        :return: psi
 
         References
         ----------
@@ -34,25 +45,23 @@ class Factor(object):
         """
         phi1 = self
         phi2 = other
-        psi_scope = sorted(list(set(phi1._scope) | set(phi2._scope)))
+        psi_scope = sorted(list(set(phi1.scope) | set(phi2.scope)))
         k_number_of_assignments = np.prod([v.card for v in psi_scope])  # Table size
-        psi_parameters = np.zeros(k_number_of_assignments)  # allocating memory
+        psi_parameters = np.zeros(k_number_of_assignments)  # Pre-allocate memory
         # index
-        j = index_for(phi1._scope, psi_scope)
-        k = index_for(phi2._scope, psi_scope)
+        j = index_for(phi1.scope, psi_scope)
+        k = index_for(phi2.scope, psi_scope)
         # multiply
         np.multiply(phi1._parameters[j], phi2._parameters[k], psi_parameters)
         return Factor(psi_scope, psi_parameters)
 
     def reordered_variables(self, new_order: list):
-        new_parameters = np.zeros(len(self._parameters))  # allocating memory
         index_scope4new_order = index_for(self._scope, new_order)
-        for i in range(len(self._parameters)):
-            new_parameters[i] = self._parameters[index_scope4new_order[i]]
+        new_parameters = np.array(self._parameters[index_scope4new_order])
         return Factor(new_order, new_parameters)
 
     def __eq__(self, other) -> bool:
-        reordered_self = self.reordered_variables(sorted(self._scope))
-        reordered_other = other.reordered_variables(sorted(other._scope))
-        return reordered_self._scope == reordered_other._scope and np.allclose(reordered_self._parameters,
-                                                                               reordered_other._parameters)
+        reordered_self = self.reordered_variables(sorted(self.scope))
+        reordered_other = other.reordered_variables(sorted(other.scope))
+        return reordered_self.scope == reordered_other.scope and np.allclose(reordered_self._parameters,
+                                                                             reordered_other._parameters)
--- classes/test_factor.py
+++ classes/test_factor.py
@@ -26,17 +26,49 @@ class TestFactor(TestCase):
         D. Koller and N. Friedman (2009). Probabilistic Graphical Models: Principles and Techniques. edited by . MIT Press.
         page 107, Figure 4.3 An example of factor product
         """
-        # Expected
         phi1_scope = [self.B, self.A]
         phi2_scope = [self.C, self.B]
-        psi_scope = [self.C, self.B, self.A]
         phi1_parameters = np.array([0.5, 0.8, 0.1, 0, 0.3, 0.9])
         phi2_parameters = np.array([0.5, 0.7, 0.1, 0.2])
-        psi_parameters = np.array([0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0., 0., 0.15, 0.21, 0.09, 0.18])
         phi1 = Factor(phi1_scope, phi1_parameters)
         phi2 = Factor(phi2_scope, phi2_parameters)
+        # Expected
+        psi_scope = [self.C, self.B, self.A]
+        psi_parameters = np.array([0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0., 0., 0.15, 0.21, 0.09, 0.18])
         psi = Factor(psi_scope, psi_parameters)
         # Actual
         results = phi1 * phi2
         results = results.reordered_variables(psi_scope)
         assert psi == results
+
+    def test_marginalization(self):
+        """
+        References
+        ----------
+        D. Koller and N. Friedman (2009). Probabilistic Graphical Models: Principles and Techniques. edited by . MIT Press.
+        page 107, Figure 4.3 An example of factor product
+        """
+        psi_scope = [self.C, self.B, self.A]
+        psi_parameters = np.array([0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0., 0., 0.15, 0.21, 0.09, 0.18])
+        psi = Factor(psi_scope, psi_parameters)
+        # Expected
+        phi3_scope = [self.C, self.A]
+        phi3_parameters = np.array([0.33, 0.51, 0.05, 0.07, 0.24, 0.39])
+        phi3 = Factor(phi3_scope, phi3_parameters)
+        # Actual
+        results = psi.marginalization([self.C, self.A])
+        results = results.reordered_variables(phi3_scope)
+        assert phi3 == results
+
+    def test_summing_out(self):
+        psi_scope = [self.C, self.B, self.A]
+        psi_parameters = np.array([0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0., 0., 0.15, 0.21, 0.09, 0.18])
+        psi = Factor(psi_scope, psi_parameters)
+        # Expected
+        phi3_scope = [self.C, self.A]
+        phi3_parameters = np.array([0.33, 0.51, 0.05, 0.07, 0.24, 0.39])
+        phi3 = Factor(phi3_scope, phi3_parameters)
+        # Actual
+        results = psi.summing_out(self.B)
+        results = results.reordered_variables(phi3_scope)
+        assert phi3 == results
